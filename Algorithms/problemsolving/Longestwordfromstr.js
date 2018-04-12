e732555728d9498dc0edf6ce5af75e93adac51ec
Zarana Patel <patelzarana@gmail.com>
2018-04-12 16:14:20 -0700

    Update Longestwordfromstr.js

--- Algorithms/problemsolving/Longestwordfromstr.js
+++ Algorithms/problemsolving/Longestwordfromstr.js
@@ -9,3 +9,21 @@ function findLongestWord(str) {
 };
 
 console.log(findLongestWord("I love javascript coding!!"));
+
+
+/* Solution 2 : 
+function LongestWord(sen){
+  var str = sen.split(" ");
+    var longest = 0;
+    var word = null;
+    for (var i = 0; i < str.length; i++) {
+        if (longest < str[i].length) {
+            longest = str[i].length;
+            word = str[i];
+        }
+    }
+    return word;
+}
+console.log(LongestWord("Testing the web application"));
+
+*/
