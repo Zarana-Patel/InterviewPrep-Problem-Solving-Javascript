57ab451cf0e1b4c773a7d23d4b642425f120fd71
Zarana Patel <patelzarana@gmail.com>
2023-04-21 13:03:56 -0700

    Update ArrayStack.js

--- Algorithms/stack/ArrayStack.js
+++ Algorithms/stack/ArrayStack.js
@@ -65,27 +65,3 @@ myStack.pop();
 console.log(myStack.peek());
 
 */
-class Stack {
-  constructor(){
-   this.array= [];
-  }
-  peek() {
-    return  this.array[this.array.length-1];
-  }
-  push(value){
-  this.array.push(value);
-  return this;
-  }
-  pop(){
-     this.array.pop();
-     return this;
-  } 
-}
-const myStack = new Stack();
-myStack.push('google');
-myStack.push('Udemy');
-console.log(myStack.push('discord'));
-console.log(myStack.peek());
-myStack.pop();
-console.log(myStack.peek());
-*/
