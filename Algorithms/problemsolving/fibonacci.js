5f4c741a4a6926d0bd71db27ea3c6643581a4305
Zarana Patel <patelzarana@gmail.com>
2023-07-19 14:46:50 -0700

    Update fibonacci.js

--- Algorithms/problemsolving/fibonacci.js
+++ Algorithms/problemsolving/fibonacci.js
@@ -1,18 +1,4 @@
 
-    /* Solution 1  display ( 0 1 1 2 3 5) */
-    var fibonacci=[];
-      fibonacci[0]=0;
-      fibonacci[1]=1;
-      for(var i=2;i<=6;i++){
-           fibonacci[i] = fibonacci[i-1]+fibonacci[i-2];
-      }
-      for(var i=0;i<fibonacci.length;i++){
-        console.log(fibonacci[i]);
-      }
-
-
-      /*Solution 2
-
        function Fibo(n){
        var fib = [];
        fib[0]=0;
