880e8d119209dd0e059e882f8a7ee51f8357c765
Zarana Patel <patelzarana@gmail.com>
2024-03-27 11:43:39 -0700

    Update FibonacciRecursion.js

--- Algorithms/Recursion/FibonacciRecursion.js
+++ Algorithms/Recursion/FibonacciRecursion.js
@@ -12,3 +12,18 @@ var fibonacci_series = function(n){
     }
 }
 console.log(fibonacci_series(4));
+
+//Recursion Way
+
+
+const fibonacci = (num) => {
+  if (num < 2) {
+    return num
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2)
+  }
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(fibonacci(i))
+}
